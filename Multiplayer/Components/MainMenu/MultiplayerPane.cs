c66ee37326e262c150ddb892f24600f458129c82
morm075 <124874578+morm075@users.noreply.github.com>
2024-06-22 10:26:05 +0930

    minor correction

--- Multiplayer/Components/MainMenu/MultiplayerPane.cs
+++ Multiplayer/Components/MainMenu/MultiplayerPane.cs
@@ -19,9 +19,12 @@ namespace Multiplayer.Components.MainMenu
     public class MultiplayerPane : MonoBehaviour
     {
         // Regular expressions for IP and port validation
+        // @formatter:off
+        // Patterns from https://ihateregex.io/
         private static readonly Regex IPv4Regex = new Regex(@"(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}");
         private static readonly Regex IPv6Regex = new Regex(@"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))");
         private static readonly Regex PortRegex = new Regex(@"^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$");
+        // @formatter:on
 
         private string ipAddress;
         private ushort portNumber;
@@ -52,7 +55,7 @@ namespace Multiplayer.Components.MainMenu
             this.RefreshData();
         }
 
-        // Token: 0x060001C2 RID: 450 RVA: 0x00007D0C File Offset: 0x00005F0C
+        // Disable listeners
         private void OnDisable()
         {
             this.SetupListeners(false);
